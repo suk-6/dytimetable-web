4072c16bb4fdc965826cfa91117706aa817bfe3c
suk-6 <me@suk.kr>
2024-06-16 11:00:58 +0900

    chore: stash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+
+import { Timetable } from "@/components/timetable";
 import { Title } from "@/components/title";
+import { TimeTableModel } from "@/models/timetable";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+	const [timetable, setTimetable] = useState<TimeTableModel>();
+	useEffect(() => {
+		fetch("/api/timetable")
+			.then((res) => res.json())
+			.then((res: TimeTableModel) => setTimetable(res));
+	}, []);
+
 	return (
 		<main className=" w-screen h-screen ">
-			<div className=" flex">
+			<div className=" flex flex-col">
 				<Title />
+				<div className=" w-full border border-gray-400" />
+				{timetable && (
+					<Timetable weekDayTimetable={timetable.student[1][1]} />
+				)}
 			</div>
 		</main>
 	);
