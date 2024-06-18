5e031a8cd47890d7aebb2a361f155c9cbeded1ea
suk-6 <me@suk.kr>
2024-06-19 08:40:04 +0900

    feat: Add mode, grade, and classroomNo state variables to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,80 @@
 import { Dropdown } from "@/components/dropdown/dropdown";
 import { Timetable } from "@/components/timetable";
 import { Title } from "@/components/title";
+import { ClassroomNo, ClassroomNoT, Grade, GradeT } from "@/models/classroom";
 import { TimeTableModel } from "@/models/timetable";
 import { useEffect, useState } from "react";
+import { BeatLoader } from "react-spinners";
 
 export default function Home() {
 	const [timetable, setTimetable] = useState<TimeTableModel>();
 	const [open, setOpen] = useState<boolean>(false);
+	const [mode, setMode] = useState<string | null>(null);
+	const [grade, setGrade] = useState<GradeT | null>(null);
+	const [classroomNo, setClassroomNo] = useState<ClassroomNoT | null>(null);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+
+	const a = ClassroomNo.Classroom1;
+
+	const refresh = async () => {
+		setMode(localStorage.getItem("mode"));
+		setGrade(
+			(() => {
+				const gradeString = localStorage
+					.getItem("classroom")
+					?.split("-")[0];
+				if (!gradeString) return null;
+
+				const grade = parseInt(gradeString);
+				return Object.values(Grade).find((g) => g === grade) || null;
+			})()
+		);
+		setClassroomNo(
+			(() => {
+				const crString = localStorage
+					.getItem("classroom")
+					?.split("-")[1];
+				if (!crString) return null;
+
+				const cr = parseInt(crString);
+				return Object.values(ClassroomNo).find((c) => c === cr) || null;
+			})()
+		);
+	};
+
 	useEffect(() => {
 		fetch("/api/timetable")
 			.then((res) => res.json())
-			.then((res: TimeTableModel) => setTimetable(res));
+			.then((res: TimeTableModel) => setTimetable(res))
+			.then(() => setIsLoading(false));
+
+		refresh();
 	}, []);
 
 	return (
 		<main className=" w-screen h-screen">
-			<div className=" flex flex-col">
-				<Title>
-					<button onClick={() => setOpen(!open)}>열기</button>
-					{open && <Dropdown setOpen={setOpen} />}
-				</Title>
-				<div className=" w-full border border-gray-400" />
-				{timetable && (
-					<Timetable
-						title="1-1"
-						weekDayTimetable={timetable.student[1][1]}
-					/>
+			<div className=" flex flex-col w-full h-full">
+				<div>
+					<Title>
+						<button onClick={() => setOpen(!open)}>열기</button>
+						{open && <Dropdown setOpen={setOpen} func={refresh} />}
+					</Title>
+					<div className=" w-full border border-gray-400" />
+				</div>
+				{isLoading ? (
+					<div className=" w-full h-full flex justify-center items-center">
+						<BeatLoader color="#494949" />
+					</div>
+				) : (
+					timetable &&
+					mode === "classroom" && (
+						<Timetable
+							title={`${grade}-${classroomNo}`}
+							weekDayTimetable={
+								timetable.student[grade!][classroomNo!]
+							}
+						/>
+					)
 				)}
 			</div>
 		</main>
