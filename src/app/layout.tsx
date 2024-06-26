0b86238f73676d8f79f354125ecc7312226fe4ea
suk-6 <me@suk.kr>
2024-06-26 09:36:54 +0900

    refactor: Update body class in layout component to use "text-nowrap" instead of "inter.className"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "덕영시간표",
@@ -20,7 +17,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="ko">
-			<body className={inter.className}>{children}</body>
+			<body className=" text-nowrap">{children}</body>
 		</html>
 	);
 }
